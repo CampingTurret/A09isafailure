385b4679e334da4a840a187a7d6fb35b7671091f
CampingTurret <l.gaeta@student.tudelft.nl>
2022-11-14 15:49:01 +0100

    Update funcmodule.py

--- funcmodule.py
+++ funcmodule.py
@@ -1,1 +1,18 @@
 
+
+def convert(value,unit):
+    
+    match unit:
+        case 1:
+            
+            convertion = float(1)
+        case 2:
+            convertion = float(2)
+
+
+    newvalue = float(value*conversion)
+
+
+    return newvalue
+    
+    
