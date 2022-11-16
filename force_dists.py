edd37e96c8d7c41eb61b32b349373ddee33d6377
Jotis4000 <pansachinis@gmail.com>
2022-11-16 16:12:30 +0100

    Update force_dists.py

--- force_dists.py
+++ force_dists.py
@@ -9,4 +9,14 @@ from aerodynamic_dist import cl_dist,cd_dist,cm_dist #Import interpolated contin
 
 q0 = 100
 q1 = 100
-chordy = 2
+y=np.linspace(0,10.1,400) #Linear discretization of continuous space, sample size=400
+
+chord_y = (ct-cr)/b*(y)+3.44 #Chord Distribution
+
+#Force Distributions
+
+CL_span=cl_dist(y)
+CD_span=cd_dist(y)
+CM_span=cm_dist(y)
+
+Lprime = CL_span*q0*chord_y
