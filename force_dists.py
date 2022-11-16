8dc2840cf9f6a8d0eb3afd79a8c9e41cf5f1a550
Jotis4000 <pansachinis@gmail.com>
2022-11-16 16:47:03 +0100

    Force dists work!

--- force_dists.py
+++ force_dists.py
@@ -1,6 +1,5 @@
 import scipy as sc
 import numpy as np
-import variables
 from variables import *
 from aerodynamic_dist import cl_dist,cd_dist,cm_dist #Import interpolated continuous distributions from data
 
@@ -13,17 +12,18 @@ from aerodynamic_dist import cl_dist,cd_dist,cm_dist #Import interpolated contin
 #Force Distributions
 
 def force_span(dist,sample,q): #Input distribution function, sample number, pressure
-    chord_y = (ct-cr)/b*(sample)+3.44 #Chord Distribution
-    ddist=dist(sample)
-    force_dist = ddist*q*chord_y
+    y=np.linspace(0,10.1,sample) #Linear discretization of continuous space
+    chord_y = (ct-cr)/b*(y)+3.44 #Chord distribution
+    ddist=dist(y) #Discretized coefficient distribution
+    force_dist = ddist*q*chord_y #Force distribution
     return force_dist
     
 def moment_span(dist,sample,q): #Input distribution function, sample number, pressure - For moment, chord is squared
-    chord_y = (ct-cr)/b*(sample)+3.44 #Chord Distribution
-    ddist=dist(sample)
-    moment_dist = ddist*q*(chord_y)^2
+    y=np.linspace(0,10.1,sample) #Linear discretization of continuous space
+    chord_y = (ct-cr)/b*(y)+3.44 #Chord Distribution
+    ddist=dist(y) #Discretized coefficient distribution
+    moment_dist = ddist*q*(chord_y)**2 #Moment distribution
     return moment_dist
-
 
 
 
--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import numpy as np
+import scipy as sc
+import variables
+from matplotlib import pyplot as plt
 
+from force_dists import *
+from aerodynamic_dist import cl_dist,cd_dist,cm_dist #Import interpolated continuous distributions from data
+
+#Vars
+sample=400
+q=7000
+
+print(cd_dist(np.linspace(0,10.1,sample)))
+
+L_prime=force_span(cl_dist,sample,q)
+D_prime=force_span(cd_dist,sample,q)
+M_prime=moment_span(cm_dist,sample,q)
+
+# plt.plot(np.linspace(0,10.1,sample),L_prime)
+# plt.plot(np.linspace(0,10.1,sample),D_prime)
+plt.plot(np.linspace(0,10.1,sample),M_prime)
+plt.show()
