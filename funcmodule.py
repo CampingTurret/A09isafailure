e412a852197205c55be0555f26ac60be21789798
Jotis4000 <pansachinis@gmail.com>
2022-11-16 15:08:08 +0100

    Update funcmodule.py

--- funcmodule.py
+++ funcmodule.py
@@ -4,9 +4,9 @@ def convert(value,unit):
     
     match unit:
         case 1:
-            convertion = float(1)
+            conversion = float(1)
         case 2:
-            convertion = float(2)
+            conversion = float(2)
 
 
     newvalue = float(value*conversion)
