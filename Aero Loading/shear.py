804019e668bddfffc9a287bfcb0f48765d3e0bf8
Jotis4000 <pansachinis@gmail.com>
2022-11-21 15:44:17 +0100

    Update shear.py

--- Aero Loading/shear.py
+++ Aero Loading/shear.py
@@ -8,6 +8,6 @@ def getShearDist(y,dist,sample):
     shear_dist=np.zeros(sample)
     i=0
     for i in range(sample):
-        shear_dist[i]=-sp.trapz(y[i:(sample-1)],dist[i:(sample-1)])
+        shear_dist[i]=-sp.trapz(dist[i:(sample-1)],y[i:(sample-1)])
         print(sp.trapz(y[i:(sample-1)],dist[i:(sample-1)]))
     return shear_dist
