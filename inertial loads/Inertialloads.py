5149d6292e9f3c38c2f78e4627a1a316df6c777e
CampingTurret <l.gaeta@student.tudelft.nl>
2022-11-21 10:01:24 +0100

    added fuel equations

--- inertial loads/Inertialloads.py
+++ inertial loads/Inertialloads.py
@@ -1,4 +1,6 @@
 import math
+import scipy
+import numpy
 
 
 def fuelvolume(A,Cr,b,labda):
@@ -11,5 +13,33 @@ def wingvolume(A,Cr,b,labda):
     return V
 
 def fuelloading(p,g,A,Cr,b,labda,y):
-    w = p * g * A * Cr * Cr *()
+
+    ratio = 1 - labda
+    w = p * g * A * Cr * Cr *(4/(b*b)*ratio * ratio * y * y -4/b * ratio * y  + 1)
     return w
+
+def fuelshear(p,g,A,Cr,b,labda,y): 
+    c1 = b/6 *(labda * labda + labda + 1)
+    ratio = 1 - labda
+    V = p * g * A * Cr * Cr *(-4/(3*b*b)*ratio * ratio * y * y * y +2/b * ratio * y * y - y + c1)
+    return 
+
+
+def fuelmomenent(p,g,A,Cr,b,labda,y): 
+    c1 = b/6 *(labda * labda + labda + 1)
+    c2 = 
+    ratio = 1 - labda
+    V = p * g * A * Cr * Cr *(-4/(3*b*b)*ratio * ratio * y * y * y +2/b * ratio * y * y - y + c1)
+    return V
+
+
+def fueltorque():
+
+
+
+    return T
+
+def structureloading(array, p, g, Cr, b ,y):
+
+
+    return W
