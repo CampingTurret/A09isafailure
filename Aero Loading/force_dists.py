8a9d11a22f6e6a7925f49eece696a6409e5c7684
JWSJacobs <J.W.S.Jacobs@student.tudelft.nl>
2022-11-21 15:18:21 +0100

    Added normal force function

--- Aero Loading/force_dists.py
+++ Aero Loading/force_dists.py
@@ -1,7 +1,8 @@
 import scipy as sc
 import numpy as np
 from variables import *
-from aerodynamic_dist import cl_dist,cd_dist,cm_dist #Import interpolated continuous distributions from data
+from aerodynamic_dist import cl_dist, cd_dist, cm_dist, alpha  # Import interpolated continuous distributions from data
+
 
 # Variables
 
@@ -9,23 +10,25 @@ from aerodynamic_dist import cl_dist,cd_dist,cm_dist #Import interpolated contin
 # q1 = 100
 # y=np.linspace(0,10.1,400) #Linear discretization of continuous space, sample size=400
 
-#Force Distributions
+# Force Distributions
 
-def force_span(dist,sample,q): #Input distribution function, sample number, pressure
-    y=np.linspace(0,10.1,sample) #Linear discretization of continuous space
-    chord_y = (ct-cr)/(b/2)*(y)+3.44 #Chord distribution
-    ddist=dist(y) #Discretized coefficient distribution
-    force_dist = ddist*q*chord_y #Force distribution
+def force_span(dist, sample, q):  # Input distribution function, sample number, pressure
+    y = np.linspace(0, 10.1, sample)  # Linear discretization of continuous space
+    chord_y = (ct - cr) / (b / 2) * (y) + 3.44  # Chord distribution
+    ddist = dist(y)  # Discretized coefficient distribution
+    force_dist = ddist * q * chord_y  # Force distribution
     return force_dist
-    
-def moment_span(dist,sample,q): #Input distribution function, sample number, pressure - For moment, chord is squared
-    y=np.linspace(0,10.1,sample) #Linear discretization of continuous space
-    chord_y = (ct-cr)/(b/2)*(y)+3.44 #Chord Distribution
-    ddist=dist(y) #Discretized coefficient distribution
-    moment_dist = ddist*q*(chord_y)**2 #Moment distribution
+
+
+def moment_span(dist, sample, q):  # Input distribution function, sample number, pressure - For moment, chord is squared
+    y = np.linspace(0, 10.1, sample)  # Linear discretization of continuous space
+    chord_y = (ct - cr) / (b / 2) * (y) + 3.44  # Chord Distribution
+    ddist = dist(y)  # Discretized coefficient distribution
+    moment_dist = ddist * q * (chord_y) ** 2  # Moment distribution
     return moment_dist
 
 
-
-
-
+def normal_span(sample, q, CLd=CL_des):  # Input distribution function, sample number, pressure, Wing lift coefficient
+    AoA = alpha(CLd)
+    normal_dist = np.cos(AoA) * force_span(cl_dist(), sample, q) + np.sin(AoA) * force_span(cd_dist(), sample, q)
+    return normal_dist
