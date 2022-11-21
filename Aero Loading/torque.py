1a0ed063927ea59521c1e757bbd3c1e3c1e7c724
areartur <A.ArendsMartinez@student.tudelft.nl>
2022-11-21 16:27:05 +0100

    Update torque.py

--- Aero Loading/torque.py
+++ Aero Loading/torque.py
@@ -1,4 +1,10 @@
 import numpy as np
 import scipy as sc
-import variables
+from variables import *
 
+# Length L, Distributed load q, distance to shear center d1, applied load P at x1, at a distance from shear center x2, a
+# distributed torque t and a torsion moment T at x2.
+# t(x) = integral from x to L of [q(x)d(x)+t(x)]dx PLUS T(1-ux1(x))+P*d2(1-ux2(x))
+
+def tau(x):
+    return sc.integrate.quad(lambda x: q(x)*d(x)+t(x), x, b/2) +
