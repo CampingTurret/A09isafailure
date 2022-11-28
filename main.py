503f1ebfaac70e71a2435cb89d6ed7c0827397ed
JWSJacobs <J.W.S.Jacobs@student.tudelft.nl>
2022-11-28 17:34:27 +0100

    legit fix last minute epic

--- main.py
+++ main.py
@@ -13,20 +13,20 @@ from aerodynamic_dist import * #Import interpolated continuous distributions fro
 
 sf=1.5 # Safety Factor
 nload=1.5*(0) # Load Factor
-
+CL = 2
 # Aerodynamic Loading
 
 y=np.linspace(0,10.1,sample)
-L_prime=force_span(cl_dist,sample,q)
-D_prime=force_span(cd_dist,sample,q)
-M_prime=moment_span(cm_dist,sample,q)
-N_prime=normal_span(sample, q, CL_des)
+L_prime=force_span(cl_dist,sample,q, CL) #* nload
+D_prime=force_span(cd_dist,sample,q, CL) #* nload
+M_prime=moment_span(cm_dist,sample,q, CL) #* nload
+N_prime=normal_span(sample, q, CL) #* nload
 
 # uniform= np.full(sample,4000)
 
 # plt.plot(np.linspace(0, 10.1, sample), L_prime)
 # plt.plot(np.linspace(0, 10.1, sample), D_prime)
-# plt.plot(np.linspace(0, 10.1, sample), N_prime)
+plt.plot(np.linspace(0, 10.1, sample), N_prime)
 # plt.plot(np.linspace(0, 10.1 ,sample), M_prime)
 
 # Inertial Loading
