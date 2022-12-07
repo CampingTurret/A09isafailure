81b4212c2297f8717c131ffe85b805abe30c05af
JWSJacobs <J.W.S.Jacobs@student.tudelft.nl>
2022-12-07 15:33:58 +0100

    Made the margin of safety function look somewhat nice

--- margin_of_safety.py
+++ margin_of_safety.py
@@ -5,11 +5,29 @@ from variables import b
 
 def plot_margin_of_safety(failure_stress, applied_stress):
     n = np.size(failure_stress)
+    print(n)
     if n == np.size(applied_stress):
         y = np.linspace(0, b/2, n)
     else:
         print("Applied stress and failure stress do not have the same size.")
         return
     margin_of_safety = failure_stress / applied_stress
-    plt.show(y, margin_of_safety)
+
+    plt.xlim([0, 11])
+    plt.xticks(np.arange(0, 12, 1.0))
+    plt.grid(True, color='0.9')
+    plt.axhline(y=0, color='black', linewidth=0.5, linestyle=(0, (5, 5)), xmax=10.1 / 11)
+    plt.xlabel('y [m]')
+    plt.ylabel('Margin of Safety')
+
+    plt.plot(y, margin_of_safety)
+
+    plt.legend(['Neutral axis', 'Margin of safety'])
+
+    plt.show()
     return
+
+a = np.ones(50) * 7
+print(a)
+c = np.linspace(10, 5, 50)
+plot_margin_of_safety(a, c)
