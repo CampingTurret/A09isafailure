67c3e714455e347bc2f37b40df3f0f3b0db93986
dani7z <danizorzos@gmail.com>
2022-12-08 02:13:48 +0100

    Create skinbuckling.py

---
+++ skinbuckling.py
@@ -0,0 +1,31 @@
+import numpy as np
+# from codeinertia import airfoilparameters as afp
+from math import pi
+
+# constants
+# y = np.linspace(0.0, 10.1, 400)
+c_r = 3.44  # chord root
+ws = 20.2  # wingspan
+E = 68.9 * 10**9
+# c = c_r - 0.6*y*2/ws*c_r
+v = 0.33
+# n_str = 1
+
+def sigmacrit(a_start, y, t, n_str):
+
+    # h1, h2, w1, w2, A = airfoilparameters(0.2, 0.75)
+
+    c = c_r - 0.6*y*2/ws*c_r
+    b = 0.55*c/(n_str + 1)
+    a = y - a_start
+    k_c = 3.277*(b**2)/(a**2)+7.125
+
+    sigma = (pi**2*k_c*E*(t**2))/(12*(1-v**2)*b**2)
+
+    return sigma, k_c, a, b
+
+
+sigma_crit = sigmacrit(0, 1, 0.003, 0)
+print(sigma_crit)
+sigma_crit2 = sigmacrit(0, 1, 0.003, 4)
+print(sigma_crit2)
