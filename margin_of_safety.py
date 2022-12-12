c4d70a82c83846fb06d69de0ddb114379e3e7a99
JWSJacobs <J.W.S.Jacobs@student.tudelft.nl>
2022-12-12 15:31:35 +0100

    Added tensile_strength.py and changed margin_of_safety.py so that it just takes a margin of safety array

--- margin_of_safety.py
+++ margin_of_safety.py
@@ -3,15 +3,15 @@ import numpy as np
 from variables import b
 
 
-def plot_margin_of_safety(failure_stress, applied_stress):
-    n = np.size(failure_stress)
-    print(n)
-    if n == np.size(applied_stress):
-        y = np.linspace(0, b/2, n)
-    else:
-        print("Applied stress and failure stress do not have the same size.")
-        return
-    margin_of_safety = failure_stress / applied_stress
+def plot_margin_of_safety(margin_of_safety):
+    # n = np.size(failure_stress)
+    # print(n)
+    # if n == np.size(applied_stress):
+    y = np.linspace(0, b/2, np.size(margin_of_safety))
+    # else:
+    #     print("Applied stress and failure stress do not have the same size.")
+    #     return
+    # margin_of_safety = failure_stress / applied_stress\
 
     plt.xlim([0, 11])
     plt.xticks(np.arange(0, 12, 1.0))
@@ -25,7 +25,7 @@ def plot_margin_of_safety(failure_stress, applied_stress):
     plt.legend(['Neutral axis', 'Margin of safety'])
 
     plt.show()
-    return
+    return margin_of_safety
 
 # a = np.ones(50) * 7
 # print(a)
