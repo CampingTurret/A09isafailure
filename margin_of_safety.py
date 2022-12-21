21b20f1a833ad2552832e6d65a7a892d2c900217
JWSJacobs <J.W.S.Jacobs@student.tudelft.nl>
2022-12-21 16:08:42 +0100

    Improved margin_of_safety.py to include bays

--- margin_of_safety.py
+++ margin_of_safety.py
@@ -3,11 +3,11 @@ import numpy as np
 from variables import b
 
 
-def plot_margin_of_safety(margin_of_safety):
+def plot_margin_of_safety(margin_of_safety, bays=False):
     # n = np.size(failure_stress)
     # print(n)
     # if n == np.size(applied_stress):
-    y = np.linspace(0, b/2, np.size(margin_of_safety))
+    y = np.linspace(0, b / 2, np.size(margin_of_safety))
     # else:
     #     print("Applied stress and failure stress do not have the same size.")
     #     return
@@ -20,16 +20,29 @@ def plot_margin_of_safety(margin_of_safety):
     plt.xlabel('y [m]')
     plt.ylabel('Margin of Safety')
 
-    plt.plot(y, margin_of_safety)
+    max_MoS = np.max(margin_of_safety)
+
+    if bays is not False:
+        np.delete(margin_of_safety, 0)
+        bays_pos = bays * y
+        #plt.bar(y, bays*max_MoS, color='0.4', width=0.08, label='Ribs')
+        plt.axvline(0, color='0.4', linestyle=(0, (5, 5)), label='Ribs')
+        for bay in bays_pos:
+            if bay != 0:
+                plt.axvline(bay, linewidth=1, color='0.4', linestyle=(0,(5,5)))
+        # plt.vlines(bays_pos, ymin=0, ymax=1, colors='0.4', label='Ribs', linestyles=(0,(5,5)))
+
+    plt.plot(y, margin_of_safety, label='Margin of safety')
     plt.gca().set_ylim(bottom=0)
 
     # plt.legend(['Neutral axis', 'Margin of safety'])
-    plt.legend(['Margin of safety'])
+    plt.legend()
 
     plt.show()
     return margin_of_safety
 
-# a = np.ones(50) * 7
-# print(a)
-# c = np.linspace(10, 5, 50)
-# plot_margin_of_safety(a, c)
+
+c = np.linspace(10, 5, 400)
+d = np.zeros(400)
+d[15], d[50], d[160], d[300], d[-1] = 1, 1, 1, 1, 1
+plot_margin_of_safety(c, d)
